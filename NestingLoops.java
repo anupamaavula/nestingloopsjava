2c8376bef4c9844b1aa0dc79a1e28e704b7c33d4
anupama <anupama_avula@yahoo.com>
2020-05-09 18:11:20 -0400

    Second commit

--- NestingLoops.java
+++ NestingLoops.java
@@ -9,13 +9,28 @@ public class NestingLoops
             {
                 System.out.println( c + " " + n );
                 //1.Look at the first set of nested loops ("CN"). Which variable changes faster? Is it the variable controlled by the outer loop (c) or the variable controlled by the inner loop (n)
-                //Ans:Variable controlled by inner loop n go faster than outer loop
+                //Ans:Variable controlled by inner loop n go faster than outer loop c
 
             }
         }
         System.out.println("\n");
 
         //2.Change the order of the loops so that the "c" loop is on the inside and the "n" loop is on the outside. How does the output change?
+//         1 A
+//         1 B
+//         1 C
+//         1 D
+//         1 E
+//         2 A
+//         2 B
+//         2 C
+//         2 D
+//         2 E
+//         3 A
+//         3 B
+//         3 C
+//         3 D
+//         3 E
         for ( int n=1; n <= 3; n++ )
         {
             for ( char c='A'; c <= 'E'; c++ )
@@ -36,6 +51,8 @@ public class NestingLoops
                 System.out.print( a + "-" + b + " " );
             }
             // * You will add a line of code here.
+            // 4.Add a System.out.println() statement after the close brace of the inner loop (the "b" loop), but still inside the outer loop. How does the output change?
+
             //System.out.println(a + "-" + b + " ");//getting an error "b coonot be resolved to a variable" because b is out of scope(outside of loop)
         }
         //3.Look at the second set of nested loops ("AB"). Change the print() statement to println(). How does the output change? 
